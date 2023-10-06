9a6c06dc26a78058fca5a457f8458c1bf785fd22
bardadymr <113371584+bardadymr@users.noreply.github.com>
2023-10-06 20:44:32 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const select = document.querySelector(".change-lang");
+const allLang = ['en', 'es', 'de', 'ua'];
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage () {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('.lang-1').innerHTML = langArr['first-header-h2'][hash];
+    document.querySelector('.lang-2').innerHTML = langArr['first-header-h3'][hash];
+    document.querySelector('.after-before').innerHTML = langArr['after'][hash];
+    document.querySelector('.sec-text').innerHTML = langArr['second-text'][hash];
+    document.querySelector('.t-text-p').innerHTML = langArr['third-text-p'][hash];
+    document.querySelector('.t-text-h3').innerHTML = langArr['third-text-h3'][hash];
+    document.querySelector('.help-h3').innerHTML = langArr['help-h2'][hash];
+    document.querySelector('.help-p').innerHTML = langArr['help-p'][hash];
+    document.querySelector('.join-h2').innerHTML = langArr['join-h2'][hash];
+    document.querySelector('.join-p').innerHTML = langArr['join-p'][hash];
+    document.querySelector('.donate').innerHTML = langArr['donate'][hash];
+    document.querySelector('.about-p').innerHTML = langArr['about'][hash];
+    document.querySelector('.foot-2').innerHTML = langArr['privacy'][hash];
+    document.querySelector('.foot-3').innerHTML = langArr['cookies'][hash];
+    document.querySelector('.foot-4').innerHTML = langArr['contact'][hash];
+
+    if(hash == 'en') {
+        document.getElementById("flag").src = "/source/britukr.png"
+    } else if (hash == 'de') {
+        document.getElementById("flag").src = "/source/gerukr.png"
+    } else if (hash == 'es') {
+        document.getElementById("flag").src = "/source/spainukr.png"
+    } else {
+        document.getElementById("flag").src = "/source/ukr.png"
+    }
+
+    
+    console.log(hash);
+    
+}
+
+changeLanguage();
---
+++ js/lang.js
@@ -0,0 +1,106 @@
+const langArr = {
+    "first-header-h2" : {
+        "en" : "They are a part of our life for us, and we are their whole life for them.",
+        "es" : "Ellos son una parte de nuestras vidas y nosotros somos para ellos toda la vida.",
+        "de" : "Sie sind für uns ein Teil unseres Lebens und wir sind für sie ihr ganzes Leben.",
+        "ua" : "Вони для нас - частина нашого життя, а ми для них - все їхнє життя.",
+    },
+
+    "first-header-h3" : {
+        "en" : "Welcome to our page!",
+        "es" : "Bienvenidos a nuestra página!",
+        "de" : "Willkommen auf unserer Seite!",
+        "ua" : "Вітаємо на нашій сторінці!",
+    },
+
+    "after" : {
+        "en" : "Before and After",
+        "es" : "Antes y después",
+        "de" : "Vorher und Nachher",
+        "ua" : "До та Після",
+    },
+
+    "second-text" : {
+        "en" : "The situation in Ukraine with homeless animals is critical. Shelters, volunteers and ordinary people who care are doing their best, but this is not enough now. The country is in flames of war. Funds are needed for food, medical treatment and equipment. Every day it becomes more and more difficult, while the number of animals does not decrease. They know and feel, they come on their own, for feeding, for help, and we try to help. Their eyes say “Help me. At least I won`t be hungry today.” We feed, treat and find homes. Therefore, we will be grateful to everyone who will take part in rescuing animals.",
+        "es" : "La situación en Ucrania con los animales sin hogar actualmente es muy crítica. Los refugios para ellos, el  voluntariado y simplemente diferentes personas hacen todo lo que está en sus manos, pero aún  así ahora mismo todo ese esfuerzo no es suficiente. El país está bajo el fuego de la guerra. Son necesarios recursos para la alimentación, el alojamiento y atención veterinaria. Cada día es cada vez más difícil y los animales en estado de desnutrición y desamparo no disminuyen. Ellos saben y sienten. Vienen de manera voluntaria en busca de alimentos algunos, otros piden ayuda y nosotros tratamos de ofrecerles toda nuestra ayuda. Sus ojos hablan: ayúdame, aunque sea hoy no moriré de hambre. Los alimentamos, los curamos y les damos amparo. Por ello, estaremos inmensamente agradecidos a todas aquellas personas de España que participen en el rescate de nuestros animales en la tierra Ucraniana.",
+        "de" : "Die Situation in der Ukraine mit obdachlosen Tieren ist kritisch. Heime, Freiwillige und fürsorgliche Menschen tun ihr Bestes, aber das reicht jetzt nicht aus. Das Land befindet sich in den Flammen des Krieges. Es werden Mittel für Lebensmittel, medizinische Behandlung und Ausrüstung benötigt. Von Tag zu Tag wird es schwieriger, während die Zahl der Tiere nicht abnimmt. Sie wissen und fühlen, sie kommen selbst zum Füttern und zum Helfen, und wir versuchen zu helfen. Ihre Augen sagen: „Hilf mir. Wenigstens werde ich heute nicht hungern&quot;. Wir füttern, behandeln und kümmern uns um Tiere.",
+        "ua" : "Ситуація з безпритульними тваринами в Україні критична. Притулки, волонтери та небайдужі люди роблять все можливе, але зараз цього недостатньо. Країна охоплена вогнем війни. Потрібні кошти на їжу, лікування та обладнання. З кожним днем стає все важче, а кількість тварин не зменшується. Вони знають і відчувають. Вони самі приходять, щоб нагодувати і допомогти, і ми намагаємося допомогти. Їхні очі говорять: Допоможіть мені. Принаймні сьогодні я не буду голодним. Ми годуємо, лікуємо і доглядаємо за тваринами.",
+    },
+
+    "third-text-p" : {
+        "en" : "We are a group of volunteers in Ukraine, despite all the horrors of the war, we help animals and those who care for them, we provide help not to shelters (!), but to ordinary citizens, old women and not only, those who take care of their pets even with their meager means. We want goodness to spread throughout the world!",
+        "es" : "Nosotros somos un grupo de voluntarios que estamos en Ucrania, que a pesar de los horrores que nos ha traído esta guerra, ayudamos a los animales y a quienes se ocupan de ellos. No en refugio!!! A simples ciudadanos, que con una miseria de medios se ocupan de sus animales domésticos. Queremos que un virus de amor y apoyo a los animales contagie al mundo entero.",
+        "de" : "Wir sind eine Gruppe von Freiwilligen in der Ukraine. Trotz aller Schrecken des Krieges helfen wir Tieren und denen, die sich um sie kümmern, nicht Tierheimen (!), sondern alleinigen Bürgern, alten Frauen und nicht nur, die sich mit ihren bescheidenen Mitteln um ihre Haustiere kümmern. Wir wollen, dass sich das Gute auf der ganzen Welt verbreitet!",
+        "ua" : "Ми - група волонтерів в Україні, незважаючи на всі жахи війни, допомагаємо тваринам і тим, хто ними опікується, ми надаємо допомогу не притулкам (!), а звичайним громадянам, бабусям і не тільки, тим, хто піклується про своїх улюбленців, навіть зі своїми мізерними коштами. Ми хочемо, щоб добро поширювалося по всьому світу!",
+    },
+
+    "third-text-h3" : {
+        "en" : "Good will definitely triumph! We do everything possible and impossible for this.",
+        "es" : "El bien, con seguridad vencerá! Y nosotros para eso hacemos todo lo que sea posible e imposible.",
+        "de" : "Das Gute wird triumphieren! Dafür tun wir alles Mögliche und Unmögliche.",
+        "ua" : "Добро обов'язково переможе! Ми робимо для цього все можливе і неможливе.",
+    },
+
+    "help-h2" : {
+        "en" : "Help in the warfare areas",
+        "es" : "Ayuda en regiones donde se llevan a cabo acciones militares",
+        "de" : "Hilfe im Gebiet der Feindseligkeiten",
+        "ua" : "Допомога в зоні бойових дій",
+    },
+
+    "help-p" : {
+        "en" : "Every week, with volunteers and fighters of the Ukrainian army, we transfer food to homeless animals in the combat area. There the situation is most critical.",
+        "es" : "Cada semana a traves del voluntariado entregamos a los combatientes del ejercito ucraniano alimentos para los animales sin hogar en zonas de combates. La situación allí es la más crítica.",
+        "de" : "Jede Woche verteilen wir durch Freiwillige und Kämpfer der ukrainischen Armee Futter an obdachlose Tiere im Kampfgebiet. Dort ist die Situation am kritischsten.",
+        "ua" : "Щотижня разом з волонтерами та бійцями української армії ми передаємо їжу безпритульним тваринам у зоні бойових дій. Там ситуація найбільш критична.",
+    },
+
+    "join-h2" : {
+        "en" : "Join now!",
+        "es" : "Únete a nosotros!",
+        "de" : "Jetzt beitreten!",
+        "ua" : "Приєднуйся до нас!",
+    },
+
+    "join-p" : {
+        "en" : "There is no small help. Even the price of one undrunk cup of coffee can save someone`s life.",
+        "es" : "Tu ayuda, por pequeña que parezca será grandiosa. Incluso el precio de una tasa de café no bebido, puede que ayuda a salvar la vida de algún animal.",
+        "de" : "Es gibt keine kleine Hilfe. Schon der Preis einer nicht getrunkenen Tasse Kaffee kann jemandem das Leben retten.",
+        "ua" : "Маленької допомоги не буває. Навіть ціна однієї невипитої чашки кави може врятувати чиєсь життя.",
+    },
+
+    "donate" : {
+        "en" : "Donate with:",
+        "es" : "Donar con:",
+        "de" : "Spende mit:",
+        "ua" : "Задонатити з:",
+    },
+
+    "about" : {
+        "en" : "CHARITABLE ORGANIZATION 'ANIMALS RESCUE UA' © 2023",
+        "es" : "ORGANIZACIÓN BENÉFICA 'ANIMALS RESCUE UA' © 2023",
+        "de" : "WOHLTÄTIGE ORGANISATION 'ANIMALS RESCUE UA' © 2023",
+        "ua" : "БЛАГОДІЙНА ОРГАНІЗАЦІЯ 'ANIMALS RESCUE UA' © 2023",
+    },
+
+    "privacy" : {
+        "en" : "Privacy Policy",
+        "es" : "Política de privacidad",
+        "de" : "Datenschutzbestimmungen",
+        "ua" : "Політика конфіденційності",
+    },
+
+    "cookies" : {
+        "en" : "Cookies Policy",
+        "es" : "Política de Cookies",
+        "de" : "Cookies-Politik",
+        "ua" : "Політика cookies",
+    },
+
+    "contact" : {
+        "en" : "Contact",
+        "es" : "Contacta",
+        "de" : "Kontakt",
+        "ua" : "Контакти",
+    },
+}
